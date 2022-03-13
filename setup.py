d3df0a1680bc32929cb20e6c72099b9fdecac1d3
xulpn <xulpn@protonmail.com>
2022-03-13 13:19:40 -0400

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import os
-print("Upgrading OS...")
+print("Setting up...\nSystem: Kali Linux \n Software: BrOwSeR_v1.0")
 os.system("sudo apt update && sudo apt upgrade")
-os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz -O ~/Downloads/geckodriver");
-os.system("chmod +x ~/Downloads/geckodriver")
-os.system("cp ~/Downloads/geckodriver /usr/local/bin")
+os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz");
+os.system("tar -xvf geckodriver-v0.30.0-linux64.tar.gz");
+os.system("sudo chmod +x geckodriver")
+os.system("sudo cp geckodriver /usr/local/bin")
+os.system("sudo rm -rf ./geckodriver*")
+
+os.system("sudo apt install python-pip && pip install selenium")
+os.system("sudo apt -y install wget gpg && sudo sh -c 'echo " + "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" + " > /etc/apt/sources.list.d/atom.list' && curl -fsSL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/atom-keyring.gpg && sudo apt update && sudo apt install atom")
+os.system("atom .")
